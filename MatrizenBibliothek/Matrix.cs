03cacb05cf3d7bd28b361338e60b9c7340dd4fc1
Paul Olbrich <Paul.Olbrich@sennheiser.com>
2021-10-29 12:52:38 +0200

    Einheits + Nullmatrix, Operationen: Addition, Multiplikation mit Matrix sowie Skalar

--- MatrizenBibliothek/Matrix.cs
+++ MatrizenBibliothek/Matrix.cs
@@ -5,14 +5,32 @@ namespace MatrizenBibliothek
     public class Matrix
     {
         double[,] chart;
-        int width;
-        int heigth;
+        public int width { get; }
+        public int heigth {  get; }
 
         public Matrix(int m, int n)
         {
             chart = new double[m, n];
             heigth = m;
             width = n;
+        }
+
+        public Matrix(int m, int n, int NullOrEinheit)
+        {
+            chart = new double[m, n];
+            heigth = m;
+            width = n;
+            if (NullOrEinheit == 0)   //erstelle Nullmatrix
+                for (int i = 0; i < heigth; i++)
+                    for (int j = 0; j < width; j++)
+                    {
+                        chart[i, j] = 0;
+                    }
+            if (NullOrEinheit == 1) //erstelle Einheitsmatrix
+                for (int i = 0; i < heigth; i++)
+                {
+                    chart[i, i] = 1;
+                }
         }
 
         public void setWert(int i, int j, double wert)
---
+++ MatrizenBibliothek/Operations.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrizenBibliothek
+{
+    public static class Operations
+    {
+        public static Matrix Addition(Matrix a, Matrix b)
+        {
+            if (a.heigth != b.heigth && a.width != b.width)
+                throw new Exception("Matrizen besitzen unterscheidliche Dimensionen.");
+
+            Matrix result = a;
+            for (int i = 0; i < a.heigth; i++)
+                for (int j = 0; j < a.width; j++)
+                    result.setWert(i, j, a.getWert(i, j) + b.getWert(i, j));
+            return result;
+        }
+
+        public static Matrix Multiplikation(Matrix a, double scalar)
+        {
+            Matrix result = a;
+            for (int i = 0; i < a.heigth; i++)
+                for (int j = 0; j < a.width; j++)
+                    result.setWert(i, j, a.getWert(i, j) * scalar);
+            return result;
+        }
+
+        public static Matrix Multiplikation(Matrix a, Matrix b)
+        {
+            if (a.width != b.heigth)
+                throw new Exception("Matrizen besitzen falsche Dimensionen.");
+
+            Matrix result = new Matrix(a.heigth, b.width);
+            for (int i = 0; i < a.heigth; i++)
+                for (int j = 0; j < a.width; j++)
+                    result.setWert(i, j, Product(a,b,i,j));
+
+                    return result;
+        }
+
+        private static double Product(Matrix a, Matrix b, int k, int l)
+        {
+            double result = 0;
+            for (int i = 0; i < a.heigth; i++)
+                for (int j = 0; j < a.width; j++)
+                     result += a.getWert(k, j) * b.getWert(i, l);
+
+            return result;
+        }
+    }
+}
