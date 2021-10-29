44d5a22cfc639e286791dc59cea677b7593168c6
Paul Olbrich <Paul.Olbrich@sennheiser.com>
2021-10-29 09:45:04 +0200

    Matrix klasse erstellt

--- MatrizenBibliothek/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace MatrizenBibliothek
-{
-    public class Class1
-    {
-    }
-}
---
+++ MatrizenBibliothek/Matrix.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MatrizenBibliothek
+{
+    public class Matrix
+    {
+        double[,] chart;
+        int width;
+        int heigth;
+
+        public Matrix(int m, int n)
+        {
+            chart = new double[m, n];
+            heigth = m;
+            width = n;
+        }
+
+        public void setWert(int i, int j, double wert)
+        {
+            if (i > heigth || j > width)
+                throw new Exception("Bereich liegt außerhalb der Matrix");
+
+            chart[i, j] = wert;
+        }
+
+        public double getWert(int i, int j)
+        {
+            if (i > heigth || j > width)
+                throw new Exception("Bereich liegt außerhalb der Matrix");
+
+            return chart[i, j];
+        }
+
+
+
+
+    }
+}
